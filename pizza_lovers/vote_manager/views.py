a01da257bf71e306dd7bac385e18eacd18a52250
deep_erx <albertodeprezzo@gmail.com>
2018-06-23 22:03:33 +0200

    barchart drawing with async db query

--- pizza_lovers/vote_manager/views.py
+++ pizza_lovers/vote_manager/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, JsonResponse
+from django.core import serializers
 
-# Create your views here.
+from vote_manager.models import Voter
+from json import dumps
+
+
+@login_required
+def get_voters_top_ten(request):
+    qs_votes = Voter.objects.values_list('vote_counter', flat=True)
+
+    # in order queryset to be serializable, convert it to list
+    ret_votes = list(qs_votes)
+
+    return JsonResponse(ret_votes,safe=False)
 
 
 @login_required
