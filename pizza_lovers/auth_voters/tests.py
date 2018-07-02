4e04d51bd2c5c60cf42d042741433a9794b7b868
deep_erx <albertodeprezzo@gmail.com>
2018-07-02 18:30:57 +0200

    unittest: logout success

--- pizza_lovers/auth_voters/tests.py
+++ pizza_lovers/auth_voters/tests.py
@@ -3,9 +3,6 @@ from django.test.client import Client
 from django.contrib.auth.models import User
 from django.urls.base import reverse
 
-# Create your tests here.
-
-
 class PizzaLoginTest(TestCase):
     def setUp(self):
         User.objects.create_user("demo_1", "test@test.com", "t3stp4ssw0rd!")
@@ -20,6 +17,8 @@ class PizzaLoginTest(TestCase):
 
         success_page = '/vote_manager/home_page/'
         result_page = response.redirect_chain[0][0]
+
+        # if logged in, client is redirected to home
         self.assertEqual(success_page, result_page)
 
     def test_login_failure(self):
@@ -32,4 +31,17 @@ class PizzaLoginTest(TestCase):
 
         failure_page = '/auth_voters/login/'
         result_page = response.redirect_chain[0][0]
+
+        # if not logged in, client is redirected to login page
         self.assertEqual(failure_page, result_page)
+
+    def test_logout(self):
+        c = Client()
+
+        response = c.get(reverse('logout'), follow=True)
+
+        success_page = '/auth_voters/login?next=/auth_voters/logout/'
+        result_page = response.redirect_chain[0][0]
+
+        # if logged out, is redirected to login with ?next= 
+        self.assertEqual(success_page, result_page)
