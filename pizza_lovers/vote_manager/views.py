5658fbf0954c36b9a88de7bc5ff3582e6d50b68e
deep_erx <albertodeprezzo@gmail.com>
2018-07-10 15:52:56 +0200

    fixing (1.vote 2.get results) order

--- pizza_lovers/vote_manager/views.py
+++ pizza_lovers/vote_manager/views.py
@@ -42,6 +42,7 @@ def send_vote(request):
     json_to_save = json.dumps(data_container)
     obj_cache.update_top_voters(json_to_save)
 
+
     status = "Another vote, thanks again!" if not created else "Your first vote, thank you!"
 
     return HttpResponse(status)
